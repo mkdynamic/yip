7695d6b01a52c0bd66d0c1a6f137ea69b4bb4d99
Mark Dodwell <markdodwell@gmail.com>
2015-09-26 21:03:45 -0700

    wip

--- lib/yip/routes.rb
+++ lib/yip/routes.rb
@@ -4,25 +4,30 @@ module Yip module Routes extend self
   def process(req, sys)
     case req[:path]
     when '/yip.js'
-      js = File.read('lib/yip/views/blimp.js')
+      js = File.read('lib/yip/resources/blimp.js')
       req
         .update_in(:res, :code) { 200 }
         .update_in(:res, &render_js(js))
     when '/yip.css'
-      css = File.read('lib/yip/views/blimp.css')
+      css = File.read('lib/yip/resources/blimp.css')
       req
         .update_in(:res, :code) { 200 }
         .update_in(:res, &render_css(css))
     when '/chat_window'
-      html = File.read('lib/yip/views/chat_window.html')
+      html = File.read('lib/yip/resources/chat_window.html')
       req
         .update_in(:res, :code) { 200 }
         .update_in(:res, &render_html(html))
     when '/chat_window.js'
-      js = File.read('lib/yip/views/chat_window.js')
+      js = File.read('lib/yip/resources/chat_window.js')
       req
         .update_in(:res, :code) { 200 }
         .update_in(:res, &render_js(js))
+    when '/chat_window.css'
+      css = File.read('lib/yip/resources/chat_window.css')
+      req
+        .update_in(:res, :code) { 200 }
+        .update_in(:res, &render_css(css))
     when '/react.js'
       js = File.read('vendor/react.js')
       req
